04c8a96d08ef7f0c71d455966ee03ce80d9d4066
Michael <bouchardm40@icloud.com>
2026-03-30 14:53:05 -0400

    Validation

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import streamlit as st
 
 a = st.text_input("Entrez un nombre", "4")
+
+try:
+    b = int(a)**2
+except ValueError:
+    b = "Vous n'avez pas entré de nombre"
+
 st.write("Le carré de votre nombre est", int(a)**2)
