fce5ba75b6f0ea091a181e0e79e89038d7c0660d
Michael <bouchardm40@icloud.com>
2026-03-30 16:24:48 -0400

    Update

--- page_1.py
+++ page_1.py
@@ -4,5 +4,5 @@ import pandas as pd
 st.title("Cocorico !")
 st.header("Analyse de chants d'oiseaux")
 st.page_link("page_2.py", label="Choisissez un fichier :")
-uploaded_file = st.file_uploader("", type="mp3")
+uploaded_file = st.file_uploader(type="mp3")
 st.button("Envoyer l'extrait")
