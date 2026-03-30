7e3b6e04a5abdb45243733a2b401702dbdfb61cc
Michael <bouchardm40@icloud.com>
2026-03-30 15:36:05 -0400

    Upload

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 import streamlit as st
+import pandas as pd
+from io import StringIO
 
-a = st.text_input("Entrez un nombre", "4")
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    # To read file as bytes:
+    bytes_data = uploaded_file.getvalue()
+    st.write(bytes_data)
 
-try:
-    st.write("Le carré de votre nombre est", float(a)**2)
-except ValueError:
-    st.write("La valeur n'est pas valide")
+    # To convert to a string based IO:
+    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    st.write(stringio)
+
+    # To read file as string:
+    string_data = stringio.read()
+    st.write(string_data)
+
+    # Can be used wherever a "file-like" object is accepted:
+    dataframe = pd.read_csv(uploaded_file)
+    st.write(dataframe)
