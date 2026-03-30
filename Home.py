3e10d4dfdf3c8ee9dc32bdb204b3dcdf917cef15
Michael <bouchardm40@icloud.com>
2026-03-30 16:19:39 -0400

    Update

--- Home.py
+++ Home.py
@@ -1,5 +1,5 @@
 # streamlit_app.py
 import streamlit as st
 
-pg = st.navigation([st.Page("page_1.py"), st.Page("page_2.py")])
+pg = st.navigation([st.Page("page_1.py"), st.Page("page_2.py")], position="hidden")
 pg.run()
