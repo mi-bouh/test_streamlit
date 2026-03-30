b3b8c5fdd59bc5d84b4a3377b88fbcf3bda409a6
Michael <bouchardm40@icloud.com>
2026-03-30 16:24:29 -0400

    Update

--- page_1.py
+++ page_1.py
@@ -3,6 +3,6 @@ import pandas as pd
 
 st.title("Cocorico !")
 st.header("Analyse de chants d'oiseaux")
-uploaded_file = st.file_uploader("Choisissez un fichier :", type="mp3")
+st.page_link("page_2.py", label="Choisissez un fichier :")
+uploaded_file = st.file_uploader("", type="mp3")
 st.button("Envoyer l'extrait")
-st.page_link("page_2.py", label="Page secrète")
