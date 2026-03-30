c461a87cf3ffe0087c3fb6500385310af223430a
Michael <bouchardm40@icloud.com>
2026-03-30 15:55:57 -0400

    Upload

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import pandas as pd
 
 st.title("Cocorico !")
 st.header("Analyse de chants d'oiseaux")
-uploaded_file = st.file_uploader("Choisissez un fichier :")
+uploaded_file = st.file_uploader("Choisissez un fichier :", type="mp3")
 st.button("Envoyer l'extrait")
