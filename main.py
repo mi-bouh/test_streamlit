0ef268299496c8e189c230f2c430a95e62668f55
Michael <bouchardm40@icloud.com>
2026-03-30 15:31:09 -0400

    Oui

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ import streamlit as st
 a = st.text_input("Entrez un nombre", "4")
 
 try:
-    st.write("Le carré de votre nombre est", int(a)**2)
+    st.write("Le carré de votre nombre est", double(a)**2)
 except ValueError:
     st.write("La valeur n'est pas valide")
