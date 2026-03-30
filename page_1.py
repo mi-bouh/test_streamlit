5bfcee9379557293c050d345c4cccabeaa022cd8
Michael <bouchardm40@icloud.com>
2026-03-30 16:35:46 -0400

    Update

--- page_1.py
+++ page_1.py
@@ -6,7 +6,7 @@ import pandas as pd
 st.title("Cocorico !")
 st.header("Analyse de chants d'oiseaux")
 st.page_link("page_2.py", label="Choisissez un fichier :")
-uploaded_file = st.file_uploader("Fichier :", type="mp3", label_visibility="hidden")
+uploaded_file = st.file_uploader("Fichier :", type="mp3", label_visibility="collapsed")
 st.button("Envoyer l'extrait")
 
 st.write(st.session_state["variable"])
--- page_2.py
+++ page_2.py
@@ -1,4 +1,3 @@
 import streamlit as st
 
 st.title("Salut !")
-st.session_state["variable"] = 8
