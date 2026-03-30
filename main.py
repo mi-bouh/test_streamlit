d3422151321e846a1fa7b4530b66a66693b8419b
Michael <bouchardm40@icloud.com>
2026-03-30 14:42:20 -0400

    Oui

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.write("Salut")
 
-a = st.text_input("Entrez un nombre", value="")
+a = st.text_input("Entrez un nombre", value="Nombre")
 
 st.write(f"Le carré de votre nombre est : {a^2}")
