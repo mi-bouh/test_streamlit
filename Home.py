75c7916596058d3170c4e48342c08026117cb43a
Michael <bouchardm40@icloud.com>
2026-03-30 16:11:21 -0400

    Upload

--- Home.py
+++ Home.py
@@ -1,5 +1,15 @@
 import streamlit as st
 
+pages = {
+    "Your account": [
+        st.Page("create_account.py", title="Create your account"),
+        st.Page("manage_account.py", title="Manage your account"),
+    ],
+    "Resources": [
+        st.Page("learn.py", title="Learn about us"),
+        st.Page("trial.py", title="Try it out"),
+    ],
+}
 
-pg = st.navigation("Page 1.py")
-pg.run
+pg = st.navigation(pages)
+pg.run()
