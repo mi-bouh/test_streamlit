1849a113ac2583a70bc2d18e40e705b1843f02ef
Michael <bouchardm40@icloud.com>
2026-04-20 17:52:25 -0400

    F

--- Oui.py
+++ Oui.py
@@ -1,7 +1,12 @@
 import streamlit as st
+from mutagen.mp3 import MP3
+
+def longueur(file):
+    return file.info.length
 
 audio = st.audio_input("Enregistrez !")
 oui = st.button("Soumettre l'enregistrement audio")
 
 if oui:
     st.audio(audio)
+    st.write(longueur(audio))
