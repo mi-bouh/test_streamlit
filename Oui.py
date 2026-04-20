485e8751b0fa08ec0feb5d74353b37d76fbb1e14
Michael <bouchardm40@icloud.com>
2026-04-20 15:59:12 -0400

    A

---
+++ Oui.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+audio_path = "C:\Users\2471794\Downloads\01 Plus tôt.mp3"
+
+st.write("Extraits audio :")
+st.audio(audio_path, format="audio/mpeg")
+a = st.text_input("Écrire ici :", "Valeur initiale")
+st.write("Vous avez écrit :", a)
