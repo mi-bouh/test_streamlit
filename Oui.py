41c75979460a112806e37f6f7034f70593636131
Michael <bouchardm40@icloud.com>
2026-04-27 16:45:23 -0400

    Oui

--- Oui.py
+++ Oui.py
@@ -5,8 +5,8 @@ audio = st.audio_input("Enregistrez !")
 # audio = "01 Plus tôt.mp3"
 oui = st.button("Soumettre l'enregistrement audio")
 duration = librosa.get_duration(path = audio)
-heures = duration / 60
+minutes = duration / 60
 
 if oui:
     st.audio(audio)
-    st.write(heures, "minutes")
+    st.write(minutes, "minutes")
