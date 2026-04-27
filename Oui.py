c44c10af8b9d6d0035ae9e05e347f0b385a70e77
Michael <bouchardm40@icloud.com>
2026-04-27 16:47:45 -0400

    Oui

--- Oui.py
+++ Oui.py
@@ -1,12 +1,15 @@
 import librosa
+import math
 import streamlit as st
 
 audio = st.audio_input("Enregistrez !")
 # audio = "01 Plus tôt.mp3"
 oui = st.button("Soumettre l'enregistrement audio")
 duration = librosa.get_duration(path = audio)
-minutes = duration / 60
+minutes = math.floor(duration / 60)
+secondes = duration - minutes
 
 if oui:
     st.audio(audio)
     st.write(minutes, "minutes")
+    st.write(secondes, "secondes")
