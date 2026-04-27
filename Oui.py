d9af3b8923f0a316fc5d13172951b5f7e18de5b7
Michael <bouchardm40@icloud.com>
2026-04-27 17:01:19 -0400

    Salut

--- Oui.py
+++ Oui.py
@@ -4,17 +4,20 @@ import streamlit as st
 audio = st.audio_input("Enregistrez !")
 oui = st.button("Soumettre l'enregistrement audio")
 oiseau = "Coucou"
-
+afficher = False
 
 if oui:
     import time
-
-    st.title("Exemple de barre de chargement")
+    afficher = True
+    if afficher:
+        st.write("Veuillez patienter...")
 
     progress_bar = st.progress(0)
 
-    for i in range(100):
-        time.sleep(0.02)
+    for i in range(200):
+        time.sleep(0.05)
         progress_bar.progress(i + 1)
 
-    st.write("Cet oiseau est : ", oiseau)
+    afficher = False
+    if afficher == False :
+        st.write("Cet oiseau est : ", oiseau)
