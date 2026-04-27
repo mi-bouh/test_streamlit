0357bb422a71beb36ba9111f7a549c2ebc84fecb
Michael <bouchardm40@icloud.com>
2026-04-27 16:58:35 -0400

    Salut

--- Oui.py
+++ Oui.py
@@ -5,5 +5,16 @@ audio = st.audio_input("Enregistrez !")
 oui = st.button("Soumettre l'enregistrement audio")
 oiseau = "Coucou"
 
+
 if oui:
+    import time
+
+    st.title("Exemple de barre de chargement")
+
+    progress_bar = st.progress(0)
+
+    for i in range(100):
+        time.sleep(0.02)
+        progress_bar.progress(i + 1)
+
     st.write("Cet oiseau est : ", oiseau)
