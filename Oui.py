51b0483ae5efaeb70aec38e38ce5404b1ed9b5e8
Michael <bouchardm40@icloud.com>
2026-04-27 16:20:15 -0400

    Oui

--- Oui.py
+++ Oui.py
@@ -1,13 +1,11 @@
+import librosa
 import streamlit as st
-from mutagen.mp3 import MP3 as mp
 
-def longueur(file):
-    return file.info.length
 
 audio = st.audio_input("Enregistrez !")
 oui = st.button("Soumettre l'enregistrement audio")
-file = mp(audio)
+
 
 if oui:
     st.audio(audio)
-    st.write(longueur(file))
+    st.write(librosa.get_duration(audio))
