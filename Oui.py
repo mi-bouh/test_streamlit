c0a1fd5ab28869f3157110a52e1a81bd511bf08f
Michael <bouchardm40@icloud.com>
2026-04-28 12:21:52 -0400

    Oups

--- Oui.py
+++ Oui.py
@@ -16,11 +16,12 @@ if "resultat" not in st.session_state:
     st.session_state.resultat = False
 
 if "nouveau" not in st.session_state:
-    st.session_state.nouveau = True
+    st.session_state.nouveau = False
 
 if st.session_state.nouveau:
     st.session_state.resultat = False
     st.session_state.progression = False
+    st.session_state.nouveau = False
 
 if not st.session_state.progression and not st.session_state.resultat:
     nouveau = False
@@ -36,7 +37,7 @@ if st.session_state.progression:
     progress_bar = st.progress(0)
 
     for i in range(100):
-        num = random.uniform(0.05, 0.15)
+        num = random.uniform(0, 0.15)
         time.sleep(num)
         progress_bar.progress(i + 1)
 
