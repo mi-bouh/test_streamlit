bc045ec94f8c5f81235b26ccaf649614155c75c6
Michael <bouchardm40@icloud.com>
2026-04-28 12:11:12 -0400

    Salut

--- Oui.py
+++ Oui.py
@@ -1,34 +1,50 @@
 import streamlit as st
 import time
+import random
 
-###
+"""
+Variables d'affichage :
+progression : Barre de progression + Texte d'attente
+resultat : Nom de l'espèce d'oiseau renvoyé par le modèle
+nouveau
+"""
 
-if "afficher" not in st.session_state:
-    st.session_state.afficher = False
+if "progression" not in st.session_state:
+    st.session_state.progression = False
+
+if "resultat" not in st.session_state:
+    st.session_state.resultat = False
 
 if "nouveau" not in st.session_state:
     st.session_state.nouveau = False
 
-if st.session_state.nouveau:
-    st.session_state.afficher = False
-
-if not st.session_state.afficher:
+if not st.session_state.progression and not st.session_state.resultat:
+    st.session_state.nouveau = False
     audio = st.audio_input("Enregistrez !")
     oui = st.button("Soumettre l'enregistrement audio")
     oiseau = "Coucou"
 
     if oui:
-        st.write("Veuillez patienter...")
+        st.session_state.progression = True
 
-        progress_bar = st.progress(0)
+if st.session_state.progression:
+    st.write("Veuillez patienter...")
 
-        for i in range(100):
-            time.sleep(0.07)
-            progress_bar.progress(i + 1)
+    progress_bar = st.progress(0)
 
-        time.sleep(0.8)
-        oui = False
+    for i in range(100):
+        num = random.uniform(0, 0.5)
+        time.sleep(0.07)
+        progress_bar.progress(i + 1)
 
-    if st.session_state.afficher:
-        st.write("Cet oiseau est : ", oiseau)
-        st.session_state.nouveau = st.button("Détecter un nouvel oiseau")
+    time.sleep(0.8)
+    st.session_state.progression = False
+    st.session_state.resultat = True
+    st.rerun()
+
+if st.session_state.resultat:
+    st.write("Cet oiseau est : ", oiseau)
+    st.session_state.nouveau = st.button("Détecter un nouvel oiseau")
+
+if st.session_state.nouveau:
+    st.session_state.resultat = False
